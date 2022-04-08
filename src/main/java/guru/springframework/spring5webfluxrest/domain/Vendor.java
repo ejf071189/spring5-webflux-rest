e585c053b8af9a9938eb8ec04c2c3734726066cf
Evan Fredericks <evan.fredericks@gmail.com>
2022-04-08 08:29:52 -0500

    vendor model/repo and data loader

--- src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
@@ -13,10 +13,11 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Category {
+public class Vendor {
 
     @Id
     private String id;
 
-    private String description;
+    private String firstName;
+    private String lastName;
 }
